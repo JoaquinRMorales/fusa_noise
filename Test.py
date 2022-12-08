82e5ea80805d6e3e025abdb43ac6d506754c2025
JoaquinRMorales <jrmoralesm97@gmail.com>
2022-12-08 20:15:42 -0300

    added modules reader at test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+import sys
+import os
+
+modules_route = str(os.getcwd()).replace("\\", "/")+"/Modules/"
+sys.path.insert(0, modules_route)
+
+import waver as wv
+
+
+wavs = wv.Wavs(0.5)
+print(wavs.metadata)
+wavs.export_data()

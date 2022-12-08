e554397aca7d607ef6bd9de92da226c553303275
JoaquinRMorales <jrmoralesm97@gmail.com>
2022-12-08 19:25:35 -0300

    Added metadata reader / writer

--- Modules/waver.py
+++ Modules/waver.py
@@ -1,4 +1,58 @@
-class Station:
+import os
+import pandas as pd
+import sys
 
-    __init__():
-        audio = ""
+from tkinter import Tk, ttk, messagebox, filedialog, Listbox, Button
+
+
+class Wavs:
+
+    def __init__(self, thrshold):
+        self.thrshold = thrshold
+        self.metadata = self.looker()
+        self.files = []
+
+    def add_file(self, file):
+        if file.duration >= self.thrshold:
+            self.files.append(file)
+
+    def looker(self):
+        root = Tk()
+        root.withdraw()
+        filename = filedialog.askopenfilename()
+
+        df = pd.read_csv(filename, sep=' ')
+
+        root.destroy()
+
+        return df
+        
+    def export_data(self):
+
+        root = Tk()
+        root.withdraw()
+        foldername = filedialog.askdirectory()
+        foldername = foldername + '/wavs_metadata.txt'
+
+        try:
+            self.metadata.to_csv(foldername, sep = ' ', mode = 'a')
+            print('Data exported!')
+
+        except :
+            print("Oops!", sys.exc_info()[0], 'occurred,')
+
+
+
+
+class File:
+
+    def __init__(self):
+        print('Hi!')
+
+
+if __name__ == "__main__":
+    wavs = Wavs(0.5)
+    print(wavs.metadata)
+    wavs.export_data()
+
+
